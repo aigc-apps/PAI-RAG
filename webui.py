fb69090fd807a9f6574b8d569d41e126338d3036
wwxxzz <zxw320697@alibaba-inc.com>
2023-09-15 19:58:54 +0800

    fix bugs

--- webui.py
+++ webui.py
@@ -92,6 +92,16 @@ async def create_config_json_file(file: UploadFile | None = None):
             cfg = json.load(c)
         _global_args.embed_model = cfg['embedding']['embedding_model']
         _global_args.vectordb_type = cfg['vector_store']
+        if 'query_topk' not in cfg:
+            cfg['query_topk'] = 4
+        if 'prompt_template' not in cfg:
+            cfg['prompt_template'] = "基于以下已知信息，简洁和专业的来回答用户的问题。如果无法从中得到答案，请说 \"根据已知信息无法回答该问题\" 或 \"没有提供足够的相关信息\"，不允许在答案中添加编造成分，答案请使用中文。\n=====\n已知信息:\n{context}\n=====\n用户问题:\n{question}"
+        if 'create_docs' not in cfg:
+            cfg['create_docs']['chunk_size'] = 200
+            cfg['create_docs']['chunk_overlap'] = 0
+            cfg['create_docs']['docs_dir'] = 'docs/'
+            cfg['create_docs']['glob'] = "**/*"
+            
         connect_time = service.init_with_cfg(cfg,_global_args)
         return {"response": "success"}
     
