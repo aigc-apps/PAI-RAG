66a06332ee1caa532b48d1496c9a74c540f4e681
筱文 <zxw320697@alibaba-inc.com>
2024-03-22 15:52:45 +0800

    supoport torch.nn.DataParallel

--- modules/LocalLLM.py
+++ modules/LocalLLM.py
@@ -3,6 +3,7 @@
 # deling.sc
 
 from transformers import AutoModel, AutoTokenizer
+import torch
 
 class LocalLLM:
     model_path = ""
@@ -13,7 +14,18 @@ class LocalLLM:
     def __init__(self, model_name_or_path):
         self.model_path = model_name_or_path
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_path, trust_remote_code=True)
-        self.model = AutoModel.from_pretrained(self.model_path, trust_remote_code=True).eval().cuda()
+        # self.model = AutoModel.from_pretrained(self.model_path, trust_remote_code=True).eval().cuda()
+        
+        # 创建模型实例
+        self.model = AutoModel.from_pretrained(self.model_path, trust_remote_code=True).eval()
+        
+        # 如果有多个GPU可用，使用DataParallel来包装模型
+        if torch.cuda.device_count() > 1:
+            print("Let's use", torch.cuda.device_count(), "GPUs!")
+            self.model = torch.nn.DataParallel(self.model)
+
+        # 将模型发送到GPU（DataParallel将自动处理设备分配）
+        self.model.cuda()
     
     def __call__(self, prompt: str):
 
