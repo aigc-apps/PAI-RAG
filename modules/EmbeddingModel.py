2eff23caaadb0f01a336b2a4b005976928a5ecd1
筱文 <zxw320697@alibaba-inc.com>
2024-03-26 11:14:37 +0800

    add model load and remove from gpu

--- modules/EmbeddingModel.py
+++ modules/EmbeddingModel.py
@@ -3,7 +3,6 @@
 # deling.sc
 
 import os
-import torch
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 
 class EmbeddingModel:
@@ -12,9 +11,3 @@ class EmbeddingModel:
         self.model_name_or_path = os.path.join(model_dir, model_name)
         self.embed = HuggingFaceEmbeddings(model_name=self.model_name_or_path,
                                            model_kwargs={'device': 'cpu'})
-
-    # def embed_query(self, query):
-    #     return self.embed.embed_query(query)
-
-    # def embed_documents(self, query):
-    #     return self.embed.embed_documents(query)
