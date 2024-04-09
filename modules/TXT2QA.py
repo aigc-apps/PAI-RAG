4f2095be582efe0d338d612ef6615fd451164600
deling.sc <deling.sc@alibaba-inc.com>
2024-04-09 17:23:09 +0800

    feat: add TXT2QA support & add choice for the 2QA modules.

---
+++ modules/TXT2QA.py
@@ -0,0 +1,28 @@
+from utils.generator import TxtGenerator
+from loguru import logger
+
+class TXT2QA:
+    def __init__(self, config):
+        self.config = config['TXTCfg']
+        self.genertor = TxtGenerator(self.config)
+    
+    def run(self, docs):
+        result = []
+        for doc in docs:
+            logger.info(f"generating qa pairs for doc:\n{doc}\n\n")
+            text_content = doc.page_content
+            qa_dict = self.genertor.generate_qa(text_content)
+            
+            result.append(qa_dict)
+        
+        return result
+
+    def del_model_cache(self):
+        if self.config['LLM'] == 'Local':
+            logger.info("Removing local llm cache from gpu memory.")
+            self.genertor.llm.del_model_cache()
+            logger.info("Clear finished.")
+
+if __name__ == "__main__":
+    x = TXT2QA()
+    x.run()
