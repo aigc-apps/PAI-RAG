5695b7ff0b2b7b5156abff31bb1803d0567c7b19
燃夏 <chenanyu.cay@alibaba-inc.com>
2024-06-03 11:46:52 +0800

    pdf_reader bug fix

--- tests/data_readers/test_easyocr_pdf_reader.py
+++ tests/data_readers/test_easyocr_pdf_reader.py
@@ -1,28 +1,27 @@
-"""
-class TestEasyOcrPdfReader(unittest.TestCase):
-    def setUp(self):
-        # load config
-        base_dir = Path(__file__).parent.parent.parent
-        config_file = os.path.join(base_dir, "src/pai_rag/config/settings.local.yaml")
-        config = RagConfiguration.from_file(config_file).get_value()
-        module_registry.init_modules(config)
-        reader_config = config["data_reader"]
-        self.directory_reader = SimpleDirectoryReader(
-            input_dir="data/pdf_data",
-            file_extractor={
-                ".pdf": PaiPDFReader(
-                    enable_image_ocr=reader_config.get("enable_image_ocr", False),
-                    model_dir=reader_config.get("easyocr_model_dir", None),
-                )
-            },
-        )
+import os
+from pathlib import Path
+from pai_rag.core.rag_configuration import RagConfiguration
+from pai_rag.modules.module_registry import module_registry
+from pai_rag.integrations.readers.pai_pdf_reader import PaiPDFReader
+from llama_index.core import SimpleDirectoryReader
+import pytest
 
-    def test_load_documents(self):
-        # load documents
-        self.documents = self.directory_reader.load_data()
-        self.assertEqual(len(self.documents), 40)
+BASE_DIR = Path(__file__).parent.parent.parent
+def test_pai_pdf_reader():
+    config_file = os.path.join(BASE_DIR, "src/pai_rag/config/settings.toml")
+    config = RagConfiguration.from_file(config_file).get_value()
+    module_registry.init_modules(config)
+    reader_config = config["data_reader"]
+    directory_reader = SimpleDirectoryReader(
+        input_dir="tests/testdata/data/pdf_data",
+        file_extractor={
+            ".pdf": PaiPDFReader(
+                enable_image_ocr=reader_config.get("enable_image_ocr", False),
+                model_dir=reader_config.get("easyocr_model_dir", None),
+            )
+        },
+    )
+    documents = directory_reader.load_data()
+    assert len(documents) > 0
 
 
-if __name__ == "__main__":
-    unittest.main()
-"""
