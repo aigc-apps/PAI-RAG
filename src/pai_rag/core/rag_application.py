435a8b596fdae91cf429072122713c402ad5981f
wwxxzz <zxw320697@alibaba-inc.com>
2024-06-19 14:37:51 +0800

    Add API to support upload local files (#67)
    * support upload file via API
    
    * add Readme for upload API
    
    * refactor query api
    
    * modify load_knowledge with session_config
    
    * use tempfile.mkdtemp() to store upload files

--- src/pai_rag/core/rag_application.py
+++ src/pai_rag/core/rag_application.py
@@ -40,9 +40,17 @@ class RagApplication:
         )
         await data_loader.aload(file_dir, enable_qa_extraction)
 
-    def load_knowledge(self, file_dir, enable_qa_extraction=False):
+    def load_knowledge(self, file_dir, faiss_path=None, enable_qa_extraction=False):
+        sessioned_config = self.config
+        if faiss_path:
+            sessioned_config = self.config.copy()
+            sessioned_config.index.update({"persist_path": faiss_path})
+            self.logger.info(
+                f"Update rag_application config with faiss_persist_path: {faiss_path}"
+            )
+
         data_loader = module_registry.get_module_with_config(
-            "DataLoaderModule", self.config
+            "DataLoaderModule", sessioned_config
         )
         data_loader.load(file_dir, enable_qa_extraction)
 
