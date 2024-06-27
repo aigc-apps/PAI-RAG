b5dc81e9e81af3e47ceef81f00ef5d8b93557432
ranxia <chenanyu.cay@alibaba-inc.com>
2024-06-27 15:41:03 +0800

    modify eas

--- src/pai_rag/integrations/llms/paieas/base.py
+++ src/pai_rag/integrations/llms/paieas/base.py
@@ -16,6 +16,7 @@ from llama_index.core.llms.custom import CustomLLM
 from llama_index.core.base.llms.generic_utils import (
     completion_to_chat_decorator,
     stream_completion_to_chat_decorator,
+    astream_completion_to_chat_decorator,
     acompletion_to_chat_decorator,
 )
 from typing import Any, Dict, Optional, Sequence
@@ -260,65 +261,103 @@ class PaiEAS(CustomLLM):
         prompt: str,
         **kwargs: Any,
     ) -> CompletionResponseAsyncGen:
-        async def gen() -> CompletionResponseAsyncGen:
-            for message in self._stream(prompt=prompt, kwargs=kwargs):
-                yield message
+        async def call_back(chunk):
+            print(f"resp: {chunk}")
+            yield chunk
 
-        # NOTE: convert generator to async generator
-        return gen()
+        params = self._default_params()
+        headers = {
+            "User-Agent": "PAI Rag Client",
+            "Authorization": f"{self.token}",
+            "Content-Type": "text/event-stream",
+        }
 
-        # params = self._default_params()
-        # headers = {"User-Agent": "PAI Rag Client", "Authorization": f"{self.token}"}
+        if self.version == "1.0":
+            pload = {"input_ids": prompt, **params}
+            # make request
+            async with httpx.AsyncClient() as http_client:
+                response = await http_client.post(
+                    url=self.endpoint, headers=headers, json=pload, timeout=60
+                )
 
-        # if self.version == "1.0":
-        #     pload = {"input_ids": prompt, **params}
-        #     # make request
-        #     async with httpx.AsyncClient() as http_client:
-        #         response = await http_client.post(
-        #             url=self.endpoint, headers=headers, json=pload, timeout=60
-        #         )
+            if response.status_code != 200:
+                raise Exception(
+                    f"Request failed with status code {response.status_code}"
+                    f" and message {response.text}"
+                )
+            res = CompletionResponse(text=response.text)
 
-        #     if response.status_code != 200:
-        #         raise Exception(
-        #             f"Request failed with status code {response.status_code}"
-        #             f" and message {response.text}"
-        #         )
-        #     res = CompletionResponse(text=response.text)
+            # yield text, if any
+            yield res
+        else:
+            pload = {
+                "prompt": prompt,
+                "use_stream_chat": "True",
+                "use_stream": True,
+                **params,
+            }
+            print("http_client pload", pload)
+            async with httpx.AsyncClient() as http_client:
+                async with http_client.stream(
+                    "POST", url=self.endpoint, headers=headers, json=pload
+                ) as resp:
+                    previous_text = ""
+                    async for raw in resp.aiter_raw():
+                        print("raw", raw)
+                        if raw:
+                            data = json.loads(raw.decode("utf-8"))
+                            text = data["response"]
 
-        #     # yield text, if any
-        #     yield res
-        # else:
-        #     pload = {"prompt": prompt, "use_stream_chat": "True", **params}
-
-        #     async with httpx.AsyncClient() as http_client:
-        #         # response = await http_client.post(
-        #         #     url=self.endpoint, headers=headers, json=pload
-        #         # )
-        #         async with http_client.stream(
-        #             "POST", url=self.endpoint, headers=headers, json=pload
-        #         ) as response:
-        #             async for chunk in response.aiter_lines():
-        #                 print("chunk", type(chunk), chunk)
-        #                 yield CompletionResponse(text="None")
+                            # yield text, if any
+                            if text:
+                                res = CompletionResponse(
+                                    text=text, delta=text[len(previous_text) :]
+                                )
+                                previous_text = text
+                                yield res
 
     @llm_completion_callback()
     async def astream_complete(
         self, prompt: str, **kwargs: Any
     ) -> CompletionResponseAsyncGen:
-        async def gen() -> CompletionResponseAsyncGen:
-            for message in self.stream_chat(prompt, **kwargs):
-                yield message
-
-        # NOTE: convert generator to async generator
-        return gen()
+        print("cd PaiEas astream_complete")
+        return self._astream(prompt=prompt, kwargs=kwargs)
 
     @llm_chat_callback()
     async def astream_chat(
         self, messages: Sequence[ChatMessage], **kwargs: Any
     ) -> ChatResponseAsyncGen:
-        async def gen() -> ChatResponseAsyncGen:
-            for message in self.stream_chat(messages, **kwargs):
-                yield message
+        print("cd PaiEas astream_chat")
+        stream_chat_fn = astream_completion_to_chat_decorator(self.astream_complete)
+        return await stream_chat_fn(messages, **kwargs)
 
-        # NOTE: convert generator to async generator
-        return gen()
+
+# from openai import AsyncOpenAI
+
+# EAS_LLM_SERVICE = " "
+# EAS_LLM_TOKENS = " "
+
+# async def test():
+#     client = AsyncOpenAI(base_url=f"{EAS_LLM_SERVICE}/v1", api_key=EAS_LLM_TOKENS)
+#     chat_stream = await client.chat.completions.create(
+#         model="default",
+#         messages=[
+#             {
+#                 "role": "system",
+#                 "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+#             },
+#             {
+#                 "role": "user",
+#                 "content": "Compose a poem that explains the concept of recursion in programming.",
+#             },
+#         ],
+#         max_tokens=512,
+#         stream=True
+#     )
+# # print(f"openai chat coompletions: {chat_stream}")
+
+#     async for chunk in chat_stream:
+#         print(f"openai chat coompletion chunk: {chunk}")
+
+# import asyncio
+# asyncio.run(test())
