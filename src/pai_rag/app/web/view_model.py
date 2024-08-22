49961e4ee468a6dfd741ef5fba3c2ccaaac8b05b
Yue Fei <luxun.fy@alibaba-inc.com>
2024-08-22 21:00:09 +0800

    Add web search (#161)
    * Add web search
    
    * Fix lint
    
    * Fix bug
    
    * Update timeout
    
    * Fix bug

--- src/pai_rag/app/web/view_model.py
+++ src/pai_rag/app/web/view_model.py
@@ -116,6 +116,11 @@ class ViewModel(BaseModel):
     retrieval_mode: str = "hybrid"  # hybrid / embedding / keyword
     query_rewrite_n: int = 1
 
+    # websearch
+    search_api_key: str = None
+    search_count: int = 10
+    search_lang: str = "zh-CN"
+
     # postprocessor
     reranker_type: str = (
         "simple-weighted-reranker"  # simple-weighted-reranker / model-based-reranker
@@ -297,6 +302,13 @@ class ViewModel(BaseModel):
             "text_qa_template", None
         )
 
+        search_config = config.get("search") or {}
+        view_model.search_api_key = search_config.get(
+            "search_api_key"
+        ) or os.environ.get("BING_SEARCH_KEY")
+        view_model.search_lang = search_config.get("search_lang", "zh-CN")
+        view_model.search_count = search_config.get("search_count", 10)
+
         return view_model
 
     def to_app_config(self):
@@ -405,6 +417,12 @@ class ViewModel(BaseModel):
 
         config["synthesizer"]["type"] = self.synthesizer_type
         config["synthesizer"]["text_qa_template"] = self.text_qa_template
+
+        config["search"]["search_api_key"] = self.search_api_key or os.environ.get(
+            "BING_SEARCH_KEY"
+        )
+        config["search"]["search_lang"] = self.search_lang
+        config["search"]["search_count"] = self.search_count
 
         return _transform_to_dict(config)
 
@@ -585,4 +603,8 @@ class ViewModel(BaseModel):
                 "value": self.get_local_evaluation_result_file(type="response")
             }
 
+        # search
+        settings["search_api_key"] = {"value": self.search_api_key}
+        settings["search_lang"] = {"value": self.search_lang}
+        settings["search_count"] = {"value": self.search_count}
         return settings
