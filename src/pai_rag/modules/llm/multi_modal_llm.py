74003f51381464d24c45409bb412d3bf9638b003
陆逊 <luxun.fy@alibaba-inc.com>
2024-08-30 10:02:01 +0800

    Fix bug

--- src/pai_rag/modules/llm/multi_modal_llm.py
+++ src/pai_rag/modules/llm/multi_modal_llm.py
@@ -1,9 +1,6 @@
 import logging
+import os
 from typing import Dict, List, Any
-from llama_index.multi_modal_llms.dashscope import (
-    DashScopeMultiModal,
-    DashScopeMultiModalModels,
-)
 
 from pai_rag.integrations.llms.multimodal.open_ai_alike_multi_modal import (
     OpenAIAlikeMultiModal,
@@ -12,6 +9,8 @@ from pai_rag.modules.base.configurable_module import ConfigurableModule
 from pai_rag.modules.base.module_constants import MODULE_PARAM_CONFIG
 
 logger = logging.getLogger(__name__)
+
+DEFAULT_DASHSCOPE_API_BASE = "https://dashscope.aliyuncs.com/compatible-mode/v1"
 
 
 class MultiModalLlmModule(ConfigurableModule):
@@ -23,7 +22,11 @@ class MultiModalLlmModule(ConfigurableModule):
         llm_config = new_params[MODULE_PARAM_CONFIG]
         if llm_config.source.lower() == "dashscope":
             logger.info("Using DashScope Multi-Modal-LLM.")
-            return DashScopeMultiModal(model_name=DashScopeMultiModalModels.QWEN_VL_MAX)
+            return OpenAIAlikeMultiModal(
+                model="qwen-vl-max",
+                api_base=DEFAULT_DASHSCOPE_API_BASE,
+                api_key=os.environ.get("DASHSCOPE_API_KEY"),
+            )
         elif llm_config.source.lower() == "paieas" and llm_config.get("endpoint"):
             logger.info("Using PAI-EAS Multi-Modal-LLM.")
             return OpenAIAlikeMultiModal(
