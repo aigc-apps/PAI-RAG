36c5b73b20e4ce3352bc25f931ffd550c32500e6
筱文 <zxw320697@alibaba-inc.com>
2024-10-17 18:00:50 +0800

    Modify result file

--- src/pai_rag/evaluation/run_evaluation_experiments.py
+++ src/pai_rag/evaluation/run_evaluation_experiments.py
@@ -3,15 +3,22 @@ import click
 import logging
 import time
 import json
+import hashlib
 from pai_rag.evaluation.run_evaluation_pipeline import run_evaluation_pipeline
+
+
+def calculate_md5_from_json(data):
+    """计算 JSON 内容的 MD5 值"""
+    hasher = hashlib.md5()
+    # 将 JSON 对象转换为字符串，并确保顺序一致
+    json_str = json.dumps(data, sort_keys=True)
+    hasher.update(json_str.encode("utf-8"))
+    return hasher.hexdigest()
 
 
 def run_experiment(exp_params):
     name = exp_params["name"]
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
-    logging.info(
-        f"Running experiment with name={name}, exp_params={exp_params}, timestamp={timestamp}"
-    )
+    logging.info(f"Running experiment with name={name}, exp_params={exp_params}")
     try:
         # 运行实验并获取结果
         result = run_evaluation_pipeline(
@@ -20,17 +27,10 @@ def run_experiment(exp_params):
             name=name,
         )
         logging.info(f"Finished experiment with name={name}")
-
-        # 记录实验结果到 JSON 文件
-        result_filename = f"localdata/eval_exp_data/results_{name}_{timestamp}.json"
-        with open(result_filename, "w") as result_file:
-            json.dump(
-                {"name": name, "parameters": exp_params, "result": result}, result_file
-            )
-        logging.info(f"Results saved to {result_filename}")
-
     except Exception as e:
         logging.error(f"Error running experiment {name}: {e}")
+
+    return {"name": exp_params["name"], "parameters": exp_params, "result": result}
 
 
 @click.command()
@@ -39,5 +39,16 @@ def run(exp_config=None):
     with open(exp_config) as file:
         configs = yaml.safe_load(file)
 
+    # 记录实验结果到 JSON 文件
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    file_key = calculate_md5_from_json(configs)
+    result_filename = f"localdata/eval_exp_data/results_{file_key}_{timestamp}.json"
+    results = []
     for exp in configs["experiment"]:
-        run_experiment(exp)
+        result = run_experiment(exp)
+        results.append(result)
+
+    with open(result_filename, "w") as result_file:
+        json.dump(results, result_file)
+
+    logging.info(f"Results saved to {result_filename}")
