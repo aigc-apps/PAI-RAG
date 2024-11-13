fcb5a17b36b139cee7ef2df83621e6e71200c1a7
筱文 <zxw320697@alibaba-inc.com>
2024-11-13 21:01:36 +0800

    test load_and_parse node

--- src/pai_rag/tools/data_process/main_workflow.py
+++ src/pai_rag/tools/data_process/main_workflow.py
@@ -39,51 +39,32 @@ def init_ray_env():
 
 
 @ray.remote
-class LockActor:
-    def __init__(self):
-        self.lock = ray.util.lock.Lock()  # 创建分布式锁
-
-    def acquire(self):
-        self.lock.acquire()  # 获取锁
-
-    def release(self):
-        self.lock.release()  # 释放锁
-
-    def write_to_file(self, data, filename):
-        with open(filename, "w", encoding="utf-8") as file:
-            json_line = json.dumps(data, ensure_ascii=False)  # 将字典转换为 JSON 字符串
-            file.write(json_line + "\n")  # 写入文件并加上换行符
+def write_to_file(data, output):
+    with open(output, "w", encoding="utf-8") as file:
+        json_line = json.dumps(data, ensure_ascii=False)  # 将字典转换为 JSON 字符串
+        file.write(json_line + "\n")  # 写入文件并加上换行符
 
 
 @ray.remote
-def process(lock_actor, input_file, output, args):
+def process(input_file, args):
     from pai_rag.tools.data_process.tasks.load_and_parse_doc_task import (
         LoadAndParseDocTask,
     )
 
     task = LoadAndParseDocTask(**vars(args))
     res = task.process(input_file)
-
-    lock_actor.acquire.remote()  # 获取锁
-    try:
-        lock_actor.write_to_file.remote(res, output)  # 写入文件
-    finally:
-        lock_actor.release.remote()  # 确保释放锁
-    return {"file": input_file, "status": "success"}
+    return res
 
 
 def main(args):
     init_ray_env()
-    lock_actor = LockActor.remote()
     output = "/app/localdata/test_LoadAndParseDocTask_ray.jsonl"
     dataset = get_dataset(args.data_path)
-    run_tasks = [process.remote(lock_actor, data, output, args) for data in dataset]
+    run_tasks = [process.remote(data, args) for data in dataset]
     results = ray.get(run_tasks)
-    with open(
-        "/app/localdata/test_LoadAndParseDocTask_status_ray.json", "w"
-    ) as status_file:
-        json.dump(results, status_file, indent=4)
-    print("Master node completed processing and saved states.")
+    print("Master node completed processing files.")
+    write_to_file.remote(results, output)
+    print(f"Results written to {output}' asynchronously.")
 
 
 if __name__ == "__main__":
