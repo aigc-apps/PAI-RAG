d11b1c6d5124028900cdb2a5d4dc7fcf46a21b15
筱文 <zxw320697@alibaba-inc.com>
2024-11-18 10:01:54 +0800

    test

--- src/pai_rag/tools/data_process/split_workflow.py
+++ src/pai_rag/tools/data_process/split_workflow.py
@@ -1,6 +1,6 @@
 import argparse
 
-# from pai_rag.tools.data_process.tasks.split_node_task import split_node_task
+from pai_rag.tools.data_process.tasks.split_node_task import split_node_task
 import ray
 from ray.data.datasource.filename_provider import _DefaultFilenameProvider
 
@@ -9,45 +9,6 @@ from ray.data.datasource.filename_provider import _DefaultFilenameProvider
 
 def init_ray_env():
     ray.init(runtime_env={"working_dir": "/PAI-RAG/"})
-
-
-def text_node_to_dict(node):
-    from datetime import datetime
-
-    return {
-        "id": node.id_,
-        "embedding": node.embedding,
-        "metadata": {
-            k: str(v) if isinstance(v, datetime) else v
-            for k, v in node.metadata.items()
-        },
-        "text": node.text,
-        "mimetype": node.mimetype,
-        "start_char_idx": node.start_char_idx,
-        "end_char_idx": node.end_char_idx,
-        "text_template": node.text_template,
-        "metadata_template": node.metadata_template,
-        "metadata_seperator": node.metadata_seperator,
-    }
-
-
-def split_node_task(document, config_file):
-    from pai_rag.core.rag_module import resolve
-    from pai_rag.integrations.nodeparsers.pai.pai_node_parser import PaiNodeParser
-    from llama_index.core.schema import Document
-
-    from pai_rag.core.rag_config_manager import RagConfigManager
-
-    config = RagConfigManager.from_file(config_file).get_value()
-    parser_config = config.node_parser
-    node_parser = resolve(cls=PaiNodeParser, parser_config=parser_config)
-    format_doc = Document(
-        doc_id=document["doc_id"],
-        text=document["data"]["content"],
-        metadata=document["data"]["meta_data"],
-    )
-    nodes = node_parser([format_doc])
-    return [text_node_to_dict(node) for node in nodes]
 
 
 def main(args):
