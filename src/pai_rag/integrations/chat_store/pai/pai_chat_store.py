c5f941cab8a8c4e1482348e268f75326f07e937c
陆逊 <luxun.fy@alibaba-inc.com>
2025-02-14 10:28:27 +0800

    Fix chat-store issues

--- src/pai_rag/integrations/chat_store/pai/pai_chat_store.py
+++ src/pai_rag/integrations/chat_store/pai/pai_chat_store.py
@@ -1,14 +1,16 @@
 from enum import Enum
 import os
+from threading import Lock
 from typing import List, Any, Literal, Optional
 from llama_index.core.llms import ChatMessage
 from llama_index.core.storage.chat_store import SimpleChatStore
 from llama_index.storage.chat_store.redis import RedisChatStore
 from llama_index.core.storage.chat_store.base import BaseChatStore
+from llama_index.core.bridge.pydantic import Field
 from pydantic import BaseModel
 from llama_index.core.bridge.pydantic import PrivateAttr
 from loguru import logger
-from collections import deque
+from collections import OrderedDict, deque
 
 CHAT_STORE_FILE = "chat_store.json"
 DEFAULT_LOCAL_STORAGE_PATH = "./localdata/storage/"
@@ -34,19 +36,21 @@ class RedisChatStoreConfig(BaseChatStoreConfig):
     type: Literal[ChatStoreType.redis] = ChatStoreType.redis
     host: str = "localhost"
     password: str = "usr:pwd"
+    ttl: int = 86400
 
 
 def create_chat_store(chat_store_config: BaseChatStoreConfig) -> BaseChatStore:
     if isinstance(chat_store_config, LocalChatStoreConfig):
         chat_file = os.path.join(chat_store_config.persist_path, CHAT_STORE_FILE)
         if not os.path.exists(chat_file):
-            return SimpleChatStore()
+            return LruSimpleChatStore()
         else:
-            return SimpleChatStore().from_persist_path(chat_file)
+            return LruSimpleChatStore().from_persist_path(chat_file)
     elif isinstance(chat_store_config, RedisChatStoreConfig):
         redis_chat_store = RedisChatStore(
             redis_url=f"redis://{chat_store_config.host}:6379",
             password=chat_store_config.password,
+            ttl=chat_store_config.ttl,
         )
         logger.info(
             "Adding Redis chat store to 'redis://{chat_store_config.host}:6379'."
@@ -54,6 +58,54 @@ def create_chat_store(chat_store_config: BaseChatStoreConfig) -> BaseChatStore:
         return redis_chat_store
     else:
         raise ValueError(f"Unknown chat store config: {chat_store_config}")
+
+
+class LruSimpleChatStore(SimpleChatStore):
+    max_message_per_session: int = Field(default=20)
+    max_session_num: int = Field(default=1000)
+    _cache: OrderedDict = PrivateAttr()
+    _lock: Lock = PrivateAttr()
+
+    def __init__(self, max_message_per_session: int = 20, max_session_num: int = 1000):
+        super().__init__()
+
+        self.max_session_num = max_session_num
+        self.max_message_per_session = max_message_per_session
+        self._cache = OrderedDict()
+        self._lock = Lock()
+
+    def add_message(self, key, message, idx=None):
+        """Add a message for a key."""
+        if idx is None:
+            self.store.setdefault(key, []).append(message)
+        else:
+            self.store.setdefault(key, []).insert(idx, message)
+
+        self.check_store_capacity(key)
+
+    def get_messages(self, key):
+        if key in self._cache:
+            self._cache.move_to_end(key)
+
+        return self.store.get(key, [])
+
+    def set_messages(self, key, messages):
+        self.store[key] = messages[-self.max_message_per_session :]
+        self.check_store_capacity(key)
+
+    def check_store_capacity(self, key):
+        if key in self._cache:
+            self._cache.move_to_end(key)
+        else:
+            self._cache[key] = True
+
+        if len(self.store[key]) > self.max_message_per_session:
+            self.store[key] = self.store[key][-self.max_message_per_session :]
+
+        if len(self.store) > self.max_session_num:
+            with self._lock:
+                session_key, _ = self._cache.popitem(last=False)
+                del self.store[session_key]
 
 
 class PaiChatStore(BaseChatStore):
@@ -76,17 +128,11 @@ class PaiChatStore(BaseChatStore):
     def get_messages(self, key: str) -> List[ChatMessage]:
         """Get messages for a key."""
         default_messages = self._chat_store.get_messages(key)
-        recent_messages = deque(default_messages[-8:], maxlen=8)
-        assistant_count = 0
+        recent_messages = deque(default_messages[-6:], maxlen=6)
         ret_messages = []
         for msg in reversed(recent_messages):
-            if msg.role == "assistant":
-                if assistant_count < 2:
-                    msg.content = msg.content[:800]
-                    ret_messages.append(msg)
-                    assistant_count += 1
-            else:
-                ret_messages.append(msg)
+            msg.content = msg.content[:600]
+            ret_messages.append(msg)
 
         ret_messages.reverse()
         return ret_messages
