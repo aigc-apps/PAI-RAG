e79aee24b0a1a961949be7857d327a5118ada2a1
陆逊 <luxun.fy@alibaba-inc.com>
2025-02-18 10:43:28 +0800

    update main

--- src/pai_rag/main.py
+++ src/pai_rag/main.py
@@ -1,8 +1,5 @@
 # init trace
 import os
-if os.getenv("ARMS_LICENSE_KEY"):
-    from aliyun.opentelemetry.instrumentation.auto_instrumentation import sitecustomize
-
 import click
 import uvicorn
 from fastapi import FastAPI
