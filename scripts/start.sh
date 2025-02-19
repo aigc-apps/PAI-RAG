7c6448a2ffa1efddf7ab99fa270042a967d5b4af
陆逊 <luxun.fy@alibaba-inc.com>
2025-02-19 13:11:46 +0800

    add port parameter

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,21 +3,24 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -w workers"
+   echo "Usage: $0 -w workers -p port"
    echo -e "\t-w number of workers, default 1"
+   echo -e "\t-p port, default 8680"
    exit 1 # Exit script after printing help
 }
 
-while getopts "w:" opt
+while getopts ":w:p:" opt
 do
    case "$opt" in
       w ) workers="$OPTARG" ;;
+      p ) port="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 workers="${workers:-1}"
+port="${port:-8680}"
 
-echo "Starting gunicorn with $workers workers..."
+echo "Starting gunicorn with $workers workers on port $port..."
 
-gunicorn -w $workers -c scripts/gunicorn.conf.py src.pai_rag.app.app:app
+gunicorn -w $workers -b "0.0.0.0:${port}" -c scripts/gunicorn.conf.py src.pai_rag.app.app:app
