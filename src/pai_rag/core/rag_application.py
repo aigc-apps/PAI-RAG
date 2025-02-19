947e79307f36672df20fdc8c40d751ba9e78514e
chuyu <chuyu.cx@alibaba-inc.com>
2025-02-19 15:38:15 +0800

    support ds_r1

--- src/pai_rag/core/rag_application.py
+++ src/pai_rag/core/rag_application.py
@@ -741,7 +741,10 @@ class RagApplication:
         }
 
         if not query.stream:
-            return RagResponse(answer=response.response, **result_info)
+            content = re.sub(
+                r"<think>.*?</think>\n*", "", response.response, flags=re.DOTALL
+            )
+            return RagResponse(answer=content, **result_info)
         else:
             return event_generator_async(
                 response=response, extra_info=result_info, sse_version=sse_version
