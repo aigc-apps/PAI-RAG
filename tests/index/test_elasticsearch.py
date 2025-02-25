b17555c7458a3f5a45925d6ca6f6efb5bc51ca14
chuyu <chuyu.cx@alibaba-inc.com>
2025-02-25 17:21:05 +0800

    fix linter

--- tests/index/test_elasticsearch.py
+++ tests/index/test_elasticsearch.py
@@ -3,14 +3,12 @@ import pytest
 from dotenv import load_dotenv
 import asyncio
 from llama_index.embeddings.dashscope import DashScopeEmbedding
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.schema import TextNode
 from pai_rag.integrations.index.pai.pai_vector_index import PaiVectorStoreIndex
-from pai_rag.integrations.index.pai.vector_store_config import ElasticSearchVectorStoreConfig
+from pai_rag.integrations.index.pai.vector_store_config import (
+    ElasticSearchVectorStoreConfig,
+)
 
-
-from llama_index.core import Document
-from llama_index.core.node_parser import SentenceSplitter
 
 # # 假设你有一个文本字符串 text_str
 # text_str1 = "dog"
@@ -34,19 +32,19 @@ mock_nodes = [
         text="Cat",
         ref_doc_id="ref_doc_1",
         doc_id="doc_1",
-        metadata={"node_id": "node_1", "ref_doc_id": "ref_1","doc_id":"doc_1"},
+        metadata={"node_id": "node_1", "ref_doc_id": "ref_1", "doc_id": "doc_1"},
     ),
     TextNode(
         text="Dog",
         ref_doc_id="ref_doc_1",
         doc_id="doc_2",
-        metadata={"node_id": "node_2", "ref_doc_id": "ref_2","doc_id":"doc_2"},
+        metadata={"node_id": "node_2", "ref_doc_id": "ref_2", "doc_id": "doc_2"},
     ),
     TextNode(
         text="Horse",
         ref_doc_id="ref_doc_2",
         doc_id="doc_3",
-        metadata={"node_id": "node_3", "ref_doc_id": "ref_3", "doc_id":"doc_3"},
+        metadata={"node_id": "node_3", "ref_doc_id": "ref_3", "doc_id": "doc_3"},
     ),
 ]
 
@@ -58,7 +56,7 @@ for i, node in enumerate(mock_nodes):
 load_dotenv()
 
 # 从环境变量中读取配置
-dashscope_key = os.getenv("DASHSCOPE_API_KEY","")
+dashscope_key = os.getenv("DASHSCOPE_API_KEY", "")
 es_host = os.getenv("es_host", "")
 es_port = "9200"
 es_user = os.getenv("es_username", "")
@@ -82,11 +80,14 @@ vector_store_index = PaiVectorStoreIndex(vector_store_config, embed_model=embed_
 # node_ids_to_delete = ["node_3"]
 # vector_store_index.delete_nodes(node_ids_to_delete)
 
+
 @pytest.mark.skipif(os.getenv("es_host") is None, reason="no host")
 def test_insert_nodes():
     # 插入三个节点
     vector_store_index.insert_nodes(mock_nodes)
-    vector_count = asyncio.run(vector_store_index._vector_store.client.count(index="pairag_test"))["count"]
+    vector_count = asyncio.run(
+        vector_store_index._vector_store.client.count(index="pairag_test")
+    )["count"]
     expected_count = len(mock_nodes)
     assert vector_count == expected_count
 
@@ -99,7 +100,8 @@ def test_delete_nodes():
     # 删除一个节点
     node_ids_to_delete = ["node_3"]
     vector_store_index.delete_nodes(node_ids_to_delete)
-    vector_count = asyncio.run(vector_store_index._vector_store.client.count(index="pairag_test"))["count"]
+    vector_count = asyncio.run(
+        vector_store_index._vector_store.client.count(index="pairag_test")
+    )["count"]
     expected_count = len(mock_nodes) - 1
     assert vector_count == expected_count
-
