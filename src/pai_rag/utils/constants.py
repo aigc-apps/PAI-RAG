d6df2d64d1b6fbee06a770d016c7b851a15645b0
wwxxzz <zxw320697@alibaba-inc.com>
2025-03-04 19:45:14 +0800

    Compatible with old index (#430)

--- src/pai_rag/utils/constants.py
+++ src/pai_rag/utils/constants.py
@@ -30,5 +30,7 @@ DEFAULT_TASK_FILE = "localdata/task_jobs.json"
 
 DEFAULT_INDEX_FILE = "localdata/default__rag__index.json"
 DEFAULT_INDEX_NAME = "default"
+DEFAULT_INDEX_NAME_OLD = "default_index"
+
 DEFAULT_MAX_INDEX_ENTRY_COUNT = os.environ.get("DEFAULT_MAX_INDEX_ENTRY_COUNT", 20)
 IGNORE_FILE_LIST = [".DS_Store"]
