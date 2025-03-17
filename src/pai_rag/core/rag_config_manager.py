17a6eea8bc85ba8836c42c7610d2b821510c0e0d
筱文 <zxw320697@alibaba-inc.com>
2025-03-17 17:04:25 +0800

    update config

--- src/pai_rag/core/rag_config_manager.py
+++ src/pai_rag/core/rag_config_manager.py
@@ -62,11 +62,16 @@ class RagConfigManager:
     def get_value(self) -> RagConfig:
         self.config.rag["llms"] = [item for item in self.config.rag["llms"] if item]
         rag_config = RagConfig.model_validate(self.config.rag)
+        rag_config_copy = rag_config
         # 兼容之前的配置
-        if rag_config.llm.is_validate():
+        if rag_config.llm.is_validate() and (
+            not rag_config_copy.llms or len(rag_config_copy.llms) == 0
+        ):
             updated_llm = rag_config.llm.copy(update={"vision_support": False})
             rag_config.llms.append(updated_llm)
-        if rag_config.multimodal_llm.is_validate():
+        if rag_config.multimodal_llm.is_validate() and (
+            not rag_config_copy.llms or len(rag_config_copy.llms) == 0
+        ):
             updated_vllm = rag_config.multimodal_llm.copy(
                 update={"vision_support": True}
             )
