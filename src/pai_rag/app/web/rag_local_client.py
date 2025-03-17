a1db18fcf37655047ebbe849735f211783f9a473
筱文 <zxw320697@alibaba-inc.com>
2025-03-17 16:25:07 +0800

    remove duplicate

--- src/pai_rag/app/web/rag_local_client.py
+++ src/pai_rag/app/web/rag_local_client.py
@@ -604,15 +604,15 @@ class RagLocalClient:
         try:
             config = rag_service.get_config()
             rag_config = RagConfig.model_validate(config)
-            # 兼容之前的配置
-            if rag_config.llm.is_validate():
-                updated_llm = rag_config.llm.copy(update={"vision_support": False})
-                rag_config.llms.append(updated_llm)
-            if rag_config.multimodal_llm.is_validate():
-                updated_vllm = rag_config.multimodal_llm.copy(
-                    update={"vision_support": True}
-                )
-                rag_config.llms.append(updated_vllm)
+            # # 兼容之前的配置
+            # if rag_config.llm.is_validate():
+            #     updated_llm = rag_config.llm.copy(update={"vision_support": False})
+            #     rag_config.llms.append(updated_llm)
+            # if rag_config.multimodal_llm.is_validate():
+            #     updated_vllm = rag_config.multimodal_llm.copy(
+            #         update={"vision_support": True}
+            #     )
+            #     rag_config.llms.append(updated_vllm)
             return rag_config
 
         except Exception as e:
