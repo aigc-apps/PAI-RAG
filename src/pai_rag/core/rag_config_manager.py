d793d8f054d98d35839c054016b11c5fe8ce60f3
陆逊 <luxun.fy@alibaba-inc.com>
2025-03-18 12:07:01 +0800

    fix init

--- src/pai_rag/core/rag_config_manager.py
+++ src/pai_rag/core/rag_config_manager.py
@@ -65,7 +65,9 @@ class RagConfigManager:
         rag_config_copy = rag_config
         # 兼容之前的配置
         if not rag_config_copy.llms or len(rag_config_copy.llms) == 0:
-            updated_llm = rag_config.llm.copy(update={"vision_support": False})
+            updated_llm = rag_config.llm.copy(
+                update={"vision_support": False, "model_id": "default"}
+            )
             rag_config.llms.append(updated_llm)
         if rag_config.multimodal_llm.is_validate() and (
             not rag_config_copy.llms or len(rag_config_copy.llms) == 0
