84f6067b07652e5feaf9d7fd168d4253b549d8bf
陆逊 <luxun.fy@alibaba-inc.com>
2025-03-20 15:25:03 +0800

    add timeout

--- src/pai_rag/core/service_daemon.py
+++ src/pai_rag/core/service_daemon.py
@@ -78,8 +78,9 @@ async def watch_knowledgebase_changes():
         change_count = 0
         for change_type, file_path in changes:
             change_count += 1
-            if change_count % 20 == 0:
+            if change_count > 20:
                 await asyncio.sleep(0.5)
+
             is_delete = change_type == 3
             try:
                 knowledgebase, change_docs = knowledgebase_manager.get_change_files(
@@ -105,6 +106,7 @@ async def watch_knowledgebase_changes():
                 logger.info(
                     f"changes enqueued. {knowledgebase}, {file_changes}, {change_type}"
                 )
+
         await asyncio.sleep(1)
 
 
