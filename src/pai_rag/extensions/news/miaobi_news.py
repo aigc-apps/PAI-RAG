c773b95b929f053220946b5d1dc3d80f1a2dd4ff
wwxxzz <zxw320697@alibaba-inc.com>
2025-03-21 14:38:30 +0800

    Personal/xiaowen/news (#464)
    * Add custom prompt for chat_news
    
    * Support llm config for news_extension

--- src/pai_rag/extensions/news/miaobi_news.py
+++ src/pai_rag/extensions/news/miaobi_news.py
@@ -133,26 +133,44 @@ def _make_context(topics):
     )
 
 
-class NewsChatParameter(BaseModel):
-    workspaceId: str
-    messages: List[Dict[str, str]] = []
-    prompt: str = None
-
-
 DEFAULT_PROMPT_TEMPLATE = """
-你是一个专业的新闻播音员。
-你会根据下面给出的新闻材料，按顺序有条理的播报所有新闻。
+# 【任务描述】你是深小闻，是一个车机新闻播报小助手。你会根据下面给出的新闻材料，按顺序有条理的播报所有新闻。
+
+# 【人设风格】风格亲切、自然但不失专业性的新闻主播深小闻。
 
 # 【精选{topics_str}新闻列表】
 {news_list_str}
 
 # 【输出格式】
-- 简短、友好的开场导语，如为您带来今天的热点资讯、为你推荐下面的科技热点等。
+- 简短、友好的开场导语，如深小闻为您带来今天的热点资讯、深小闻为你推荐下面的科技热点等。
 - 保持亲切、自然的语言风格同时不失专业性。
 - 请根据新闻列表中信息播报，不要使用其他信息。
 - 请遵循新闻给出的顺序，结构化、有条理的归纳每条新闻内容并用数字序号标识。
 - 注意每条新闻播报不要超过100个字。
 """
+
+DEFAULT_CHAT_CUSTOM_PROMPT_TEMPLATE = """
+# 【任务描述】你是深小闻，一个智能车机问答助手，你的职责是根据给定的上下文信息回答问题。
+
+# 【上下文信息】
+{content}
+
+# 【人设风格】风格亲切、自然但不失专业性的新闻主播深小闻
+
+# 【输出格式】
+- 请根据上下文信息，不要使用其他信息，参考【人设风格】，结构条理化的回答问题“{prompt}”。
+- 回答时使用简短、友好的开场导语，如深小闻为您带来关于（）的热点新闻.
+- 内容的字数一定控制在{answerLength}个字符以内。
+- 如果不能回答，请输出：根据已知信息无法回答。
+"""
+
+
+class NewsChatParameter(BaseModel):
+    workspaceId: str
+    messages: List[Dict[str, str]] = []
+    prompt: str = None
+    modelCustomPromptTemplate: str = DEFAULT_CHAT_CUSTOM_PROMPT_TEMPLATE
+    answerLength: int = 200
 
 
 class MiaobiNewsTool:
@@ -354,7 +372,7 @@ class MiaobiNewsTool:
                 delta="",
                 additional_kwargs={"intent": ChatIntentType.CHAT_NEWS},
             )
-
+            logger.info(f" Chat news with param {param}.")
             async for item in await self.chat_client.do_sse_query(param):
                 try:
                     data = json.loads(item.get("event").data)
