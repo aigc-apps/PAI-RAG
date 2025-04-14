efc4988cf0f31db5bbaecf8b2cfefd8885e6943b
wwxxzz <zxw320697@alibaba-inc.com>
2025-04-14 11:38:05 +0800

    Restrict news domains (#500)
    * Restrict news domains
    
    * Add more test cases

--- src/pai_rag/integrations/query_transform/pai_query_transform.py
+++ src/pai_rag/integrations/query_transform/pai_query_transform.py
@@ -1,7 +1,10 @@
 from typing import List, Optional, Sequence
 from llama_index.core.llms.utils import LLMType
 from llama_index.core.base.llms.types import ChatMessage
-from pai_rag.extensions.news.news_config import DEFAULT_NEWS_DOMAIN_LIST
+from pai_rag.extensions.news.news_config import (
+    DEFAULT_NEWS_DOMAIN_LIST,
+    DEFAULT_NEWS_DOMAIN_MAP,
+)
 from pai_rag.utils.prompt_template import (
     KNOWLEDGEBASE_REWRITE_PROMPT_ZH,
     CHAT_LLM_REWRITE_PROMPT_ZH,
@@ -41,6 +44,11 @@ def messages_to_history_str(
         string_messages.append(string_message)
 
     return "\n".join(string_messages)
+
+
+def check_keywords_in_string(string, keywords):
+    # 使用 any() 函数检查是否有任何一个关键字出现在字符串中
+    return any(keyword in string for keyword in keywords)
 
 
 class OpenAICompatibleQueryTransform:
@@ -125,10 +133,20 @@ class OpenAICompatibleQueryTransform:
         query = query_json.get("query", query_str)
         news_topics = query_json.get("news_topics", [])
 
-        # 过滤掉无关话题
-        filtered_news_topics = [
-            topic for topic in news_topics if topic in set(self._news_valid_domain_list)
-        ]
+        # 过滤掉无关话题 并且 进行严格的落域字符串匹配
+        # filtered_news_topics = [
+        #     topic for topic in news_topics if topic in set(self._news_valid_domain_list)
+        # ]
+        filtered_news_topics = []
+        for topic in news_topics:
+            if topic in set(self._news_valid_domain_list) and check_keywords_in_string(
+                query_str, DEFAULT_NEWS_DOMAIN_MAP[topic]
+            ):
+                logger.debug(f"Valid news topic [{topic}]")
+                filtered_news_topics.append(topic)
+            else:
+                logger.debug(f"Invalid news topic [{topic}]")
+        logger.debug(f"Filtered news topics [{filtered_news_topics}]")
         if (
             len(news_topics) > 0
             and len(filtered_news_topics) == 0
