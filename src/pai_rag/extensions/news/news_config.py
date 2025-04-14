ac4ccc7c3c3e3d74f47b7130d73ac24eeba958a8
wwxxzz <zxw320697@alibaba-inc.com>
2025-04-14 20:09:43 +0800

    Add config for news_role (#501)

--- src/pai_rag/extensions/news/news_config.py
+++ src/pai_rag/extensions/news/news_config.py
@@ -6,16 +6,18 @@ from pai_rag.integrations.llms.pai.llm_config import (
 DEFAULT_NEWS_ENDPOINT = "quanmiaolightapp.cn-beijing.aliyuncs.com"
 DEFAULT_TOP_NEWS_COUNT = 10
 DEFAULT_CHAT_NEWS_ANSWER_LEN = 200
-DEFAULT_PROMPT_TEMPLATE = """
-# 【任务描述】你是深小闻，是一个车机新闻播报小助手。你会根据下面给出的新闻材料，按顺序有条理的播报所有新闻。
+DEFAULT_NEWS_ROLE_SETTING = "新闻小助手"
 
-# 【人设风格】风格亲切、自然但不失专业性的新闻主播深小闻。
+DEFAULT_PROMPT_TEMPLATE = """
+# 【任务描述】你是{news_role}，是一个车机新闻播报小助手。你会根据下面给出的新闻材料，按顺序有条理的播报所有新闻。
+
+# 【人设风格】风格亲切、自然但不失专业性的新闻主播{news_role}。
 
 # 【精选{topics_str}新闻列表】
 {news_list_str}
 
 # 【输出格式】
-- 简短、友好的开场导语，如深小闻为您带来今天的热点资讯、深小闻为你推荐下面的科技热点等。
+- 简短、友好的开场导语，如{news_role}为您带来今天的热点资讯、{news_role}为你推荐下面的科技热点等。
 - 保持亲切、自然的语言风格同时不失专业性。
 - 请根据新闻列表中信息播报，不要使用其他信息。
 - 请遵循新闻给出的顺序，结构化、有条理的归纳每条新闻内容并用数字序号标识。
@@ -25,16 +27,16 @@ DEFAULT_PROMPT_TEMPLATE = """
 """
 
 DEFAULT_CHAT_CUSTOM_PROMPT_TEMPLATE = """
-# 【任务描述】你是深小闻，一个智能车机问答助手，你的职责是根据给定的上下文信息回答问题。
+# 【任务描述】你是{news_role}，一个智能车机问答助手，你的职责是根据给定的上下文信息回答问题。
 
 # 【上下文信息】
 {content}
 
-# 【人设风格】风格亲切、自然但不失专业性的新闻主播深小闻
+# 【人设风格】风格亲切、自然但不失专业性的新闻主播{news_role}
 
 # 【输出格式】
 - 请根据上下文信息，不要使用其他信息，参考【人设风格】，结构条理化的回答问题“{prompt}”。
-- 回答时使用简短、友好的开场导语，如深小闻为您带来关于（）的热点新闻.
+- 回答时使用简短、友好的开场导语，如{news_role}为您带来关于（）的热点新闻.
 - 内容的字数一定控制在{answerLength}个字符以内。
 - 如果不能回答，请输出：根据已知信息无法回答。
 - 注意回答中不要提到“从参考内容得出”、“从材料得出”等字眼，不要包含链接内容。
@@ -42,7 +44,7 @@ DEFAULT_CHAT_CUSTOM_PROMPT_TEMPLATE = """
 
 
 DEFAULT_NEWS_ROLE = """
-Hi，我是深小闻，\n 您的车载 AI 新闻助手。\n 我可以为你进行各个领域的新闻热点播报，\n还可以和你互动、\n根据你的问题进行新闻详细介绍与解析。\n 想知道有哪些新闻热点，\n 尽管来问我。\n
+Hi，我是{news_role}，\n 您的车载 AI 新闻助手。\n 我可以为你进行各个领域的新闻热点播报，\n还可以和你互动、\n根据你的问题进行新闻详细介绍与解析。\n 想知道有哪些新闻热点，\n 尽管来问我。\n
 温馨提示，\n 你可以问我 \n【{domain_list}】\n 这些领域的新闻哦~\n也可以直接问我你感兴趣的某条新闻，\n快来体验吧。\n
 """
 DEFAULT_NEWS_ERROR_MESSAGE = "抱歉，查询新闻发生错误，请稍后重试。"
@@ -80,6 +82,7 @@ class MiaobiNewsConfig(BaseModel):
     list_topics_prompt_str: str = DEFAULT_PROMPT_TEMPLATE
     chat_news_prompt_str: str = DEFAULT_CHAT_CUSTOM_PROMPT_TEMPLATE
     domain_list: list[str] = DEFAULT_NEWS_DOMAIN_LIST
+    news_role: str = DEFAULT_NEWS_ROLE_SETTING
 
     def is_enabled(self) -> bool:
         return (
