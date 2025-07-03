3b914c1c5f1a5de3c5b216bf815231470d4e2ed2
燃夏 <chenanyu.cay@alibaba-inc.com>
2025-07-03 17:25:15 +0800

    fix memory history messages

--- src/pairag/memory/base_memory.py
+++ src/pairag/memory/base_memory.py
@@ -51,7 +51,16 @@ class BaseMemory:
         else:
             self.history_messages.append(msgs[0])
             self.history_tokens_num += estimate_tokens_in_message(msgs[0])
-        for message in get_last_n_msgs_skip_first(msgs, DEFAULT_HISTORY_MESSAGES_COUNT):
+        last_n_history_messages = get_last_n_msgs_skip_first(
+            msgs, DEFAULT_HISTORY_MESSAGES_COUNT
+        )
+        # 如果第一个message是tool,"tool_calls" message必须在他前面, 成对出现。删除掉tool message
+        if (
+            last_n_history_messages
+            and last_n_history_messages[0].role == MessageRole.TOOL
+        ):
+            del last_n_history_messages[0]
+        for message in last_n_history_messages:
             new_msg, tokens_num = self.truncate_message(
                 message, max_tokens=DEFAULT_HISTORY_MESSAGES_INPUT_TOKENS
             )
