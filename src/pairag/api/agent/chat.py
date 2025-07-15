882047254024d5b1500913e9c9e327ce45626524
Ceceliachenen <chenanyu.cay@alibaba-inc.com>
2025-07-15 17:35:27 +0800

    answer dumps&eval demo (#606)
    * answer dump
    
    * update eval demo
    
    * add eval demo
    
    * fix lint
    
    * fix eval demo
    
    * refine

--- src/pairag/api/agent/chat.py
+++ src/pairag/api/agent/chat.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, Response
 from fastapi.responses import StreamingResponse
 from pairag.mcp.agent_loop import AgentLoop
 from pairag.mcp.models import ChatAgentRequest
-from pairag.mcp.stream_text import VercelAiDataStreamWriter
+from pairag.mcp.stream_text import VercelAiDataStreamWriter, AgentFinalAnswerWriter
 import traceback
 from loguru import logger
+import time
 
 
 chat_agent_router = APIRouter()
@@ -24,6 +25,35 @@ async def chat(chat_request: ChatAgentRequest):
             headers={"x-vercel-ai-data-stream": "v1"},
         )
 
+    except Exception:
+        logger.exception(f"Error in /api/chat: {traceback.format_exc()}")
+        return Response(content="Internal Server Error", status_code=500)
+
+
+agent_answer_dump_router = APIRouter()
+
+
+@agent_answer_dump_router.post("")
+async def get_final_answer(chat_request: ChatAgentRequest):
+    logger.info(f"Chat agent body: {chat_request}")
+    try:
+        start_time = time.time()
+        agent_loop = AgentLoop()
+        async_response_gen = await agent_loop.arun(chat_request=chat_request)
+
+        final_answer_writer = AgentFinalAnswerWriter()
+        final_answer, step = await final_answer_writer.astream_text(async_response_gen)
+        end_time = time.time()
+        run_time = end_time - start_time
+        logger.info(
+            f"Final answer: {final_answer}, Step: {step}, Run time: {run_time:.1f}s"
+        )
+        final_answer_dict = {
+            "answer": final_answer,
+            "step": step,
+            "run_time": round(run_time, 1),
+        }
+        return final_answer_dict
     except Exception:
         logger.exception(f"Error in /api/chat: {traceback.format_exc()}")
         return Response(content="Internal Server Error", status_code=500)
