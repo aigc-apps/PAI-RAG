062032b8c31458d086848c7ac05a27b41cdb3329
燃夏 <chenanyu.cay@alibaba-inc.com>
2025-11-06 11:51:31 +0800

    update attachments

--- backend/rag/chunk_helper.py
+++ backend/rag/chunk_helper.py
@@ -170,6 +170,9 @@ async def update_file_content_async(
             if len(file.file_content) > DEFAULT_ATTACHMENT_MAX_SIZE:
                 file.file_content = file.file_content[0:DEFAULT_ATTACHMENT_MAX_SIZE] + " \n\n [truncated] The content is too long, has been truncated."
             file.file_content_length = len(file.file_content)
+        else:
+            file.file_content = None
+            file.file_content_length = 0
     try:
         session.add(file)
         await session.commit()
